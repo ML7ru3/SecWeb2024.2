560ca6914dcf8200f3f035e482c8e034d6861520
DeiET0702 <your@email.com>
2025-05-16 18:47:32 +0700

    update

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,13 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { test, registerUser, loginUser, getProfile, logoutUser, updateUser, getAllUsers, deleteUser, resetUserScore, addUserByAdmin } = require('../controllers/authController');
 const { requireAuth, requireAdmin, registerLimiter, loginLimiter, adminUsersLimiter, updateLimiter } = require('../helpers/auth');
-const cors = require('cors');
-const Recaptcha = require('express-recaptcha').RecaptchaV3;
-
-// Cấu hình reCAPTCHA v3
-const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY, {
-  callback: 'verifyCallback'
-});
 
 // Routes
 router.get('/', test);
